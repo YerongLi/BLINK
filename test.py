425fad4cb4f9ba0d4f441b8366a5caf76cd55073
Yerong <yerong.li@outlook.com>
2021-07-06 19:11:00 -0500

    blink

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ config = {
     "test_entities": None,
     "test_mentions": None,
     "interactive": False,
-    "top_k": 1000,
+    "top_k": 100,
     "biencoder_model": models_path+"biencoder_wiki_large.bin",
     "biencoder_config": models_path+"biencoder_wiki_large.json",
     "entity_catalogue": models_path+"entity.jsonl",
@@ -30,14 +30,14 @@ data_to_link = [ {
                     "mention": "Shakespeare".lower(),
                     "context_right": "'s account of the Roman general Julius Caesar's murder by his friend Brutus is a meditation on duty.".lower(),
                 },
-                {
-                    "id": 1,
-                    "label": "unknown",
-                    "label_id": -1,
-                    "context_left": "Shakespeare's account of the Roman general".lower(),
-                    "mention": "Julius Caesar".lower(),
-                    "context_right": "'s murder by his friend Brutus is a meditation on duty.".lower(),
-                }
+                # {
+                #     "id": 1,
+                #     "label": "unknown",
+                #     "label_id": -1,
+                #     "context_left": "Shakespeare's account of the Roman general".lower(),
+                #     "mention": "Julius Caesar".lower(),
+                #     "context_right": "'s murder by his friend Brutus is a meditation on duty.".lower(),
+                # }
                 ]
 
 _, _, _, _, _, predictions, scores, = main_dense.run(args, None, *models, test_data=data_to_link)
