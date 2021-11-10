6dfecfd6d4a12c0e0ba093d9c055ce6ced7b8806
Yerong Li <yerong.li@outlook.com>
2021-11-10 13:04:51 -0600

    Printing: pick the first 10 training data

--- blink/biencoder/train_biencoder.py
+++ blink/biencoder/train_biencoder.py
@@ -161,9 +161,10 @@ def main(params):
         logger=logger,
         debug=params["debug"],
     )
-    for item in train_data:
-        print(item)
-    sys.exit()
+    print(train_data)
+    # for item in train_data:
+    #     print(item)
+    # sys.exit()
 
     if params["shuffle"]:
         train_sampler = RandomSampler(train_tensor_data)
